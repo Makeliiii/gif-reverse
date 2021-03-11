7ce30119c83b1349e73c24fb4f56ae8b493fc9d4
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-11 12:36:25 +0200

    we get an extremely warped reversed gif now

--- index.go
+++ index.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/gif"
 	"log"
 	"os"
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	readPath := "gif/booba.gif"
-	writePath := "gif/booba.jpeg"
+	writePath := "gif/boobaRev.gif"
 	reader, err := os.Open(readPath)
 	if err != nil {
 		log.Fatal(err)
@@ -20,10 +21,25 @@ func main() {
 		log.Fatal(errrr)
 	}
 
-	image, error := gif.Decode(reader)
+	images, error := gif.DecodeAll(reader)
 	if error != nil {
 		log.Fatal(error)
 	}
 
-	gif.Encode(writer, image, &gif.Options{NumColors: 256})
+	//revArr := make([]image.PalettedImage, 0, cap(images.Image))
+
+	//fmt.Println("Before loop: ", images)
+
+	newGif := gif.GIF{}
+	newGif.LoopCount = 0
+
+	for i := len(images.Image) - 1; i > -1; i-- {
+		newGif.Image = append(newGif.Image, images.Image[i])
+		newGif.Delay = append(newGif.Delay, images.Delay[i])
+		newGif.Disposal = append(newGif.Disposal, images.Disposal[i])
+	}
+
+	fmt.Println("After loop:", newGif)
+
+	gif.EncodeAll(writer, &newGif)
 }
